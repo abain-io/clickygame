f7ee49baa35ffc4f28b84a3ec312fb0dc6217e57
dolphinman <wpfan.org@gmail.com>
2018-06-25 13:15:58 -0500

    added updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header-wrapper">
-        <h1 className="start-game-header-text">Click On An Image To Start</h1>
+        <h1 className="start-game-header-text">Click On An Image To Begin</h1>
         <br />
-        <h4>The goal of the game is to click on all of the pictures but no more than once</h4>
+        <h4>The goal of the game is to click on all of the images without clicking on the same image twice</h4>
       </div>
     );
   }
